0644bac3236aec850d35c801268f21dd1a184bc0
sheldonv <velazquezsheldon@gmail.com>
2021-03-03 23:18:07 -0800

    ready app for deployment

---
+++ routes/login.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const loginRouter = express.Router();
+const Profile = require('../models/profile');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
+
+loginRouter.post('', (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    let fetchedProfile;
+    Profile.findOne({email: email}).then(
+        (profile) => {
+            if(!profile){
+                return res.status(500).json({
+                    message: 'Profile not found',
+                    auth: false
+                })
+            }else{
+                 fetchedProfile = profile;
+                 return bcrypt.compare(password, profile.password)
+            }
+        }
+    ).then(
+        (results) => {
+            if(!results){
+                return res.status(404).json({message: 'Password not Valid'})
+            }
+            const token = jwt.sign({name: fetchedProfile.name, id: fetchedProfile._id},
+                                    process.env.JWT_KEY,
+                                     {expiresIn: '1h'})
+            res.status(200).json({profile: fetchedProfile, auth: true, token: token})
+        }
+    )
+})
+module.exports = loginRouter;
